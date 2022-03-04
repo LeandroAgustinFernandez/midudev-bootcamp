e34c47b65ac44108a7aa09ae04e0194f5f61d0ac
= <=>
2022-03-04 19:57:50 -0300

    Parte 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Message from "./Message.js";
+import Description from "./Description.js";
 
 function App() {
+  // const mensaje = "Retornando un mensaje desde la variable";
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Siempre se deben cerrar las etiquetas</p>
+      <br />
+      <Message color='red' message='Estamos trabajando'/>
+      <Message color='yellow' message='En un curso'/>
+      <Message color='blue' message='De react'/>
+      {/* <h2>{mensaje + " con concatenacion"}</h2> */}
+      <Description />
     </div>
   );
 }
+// OTRA FORMA VALIDA DE CREAR LA FUNCION.
+// const App = () => {
+//   return (
+//     <div className="App">
+//       <h1>Hola Mundo</h1>
+//     </div>
+//   );
+// };
 
 export default App;
---
+++ src/Description.js
@@ -0,0 +1,5 @@
+const Description = () => {
+  return <p>Este es un curso de ReactJs FrontEnd y BackEnd</p>;
+};
+
+export default Description;
